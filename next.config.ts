4debcb29fa0e53efcd1665ab87d25fee2501611e
Kabir Aremu <bittyzens@gmail.com>
2025-07-28 17:00:15 +0100

    next config modified

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,26 @@
 import type { NextConfig } from "next";
 
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [
+//       {
+//         protocol: 'https',
+//         hostname: '**',
+//       },
+//       {
+//         protocol: 'http',
+//         hostname: '**',
+//       },
+//     ],
+//   },
+
+
+// };
+
+
+// export default nextConfig;
+
+// next.config.ts
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
@@ -13,6 +34,24 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+
+  experimental: {
+    serverComponentsExternalPackages: ['@prisma/client'],
+  },
+
+  outputFileTracingIncludes: {
+    // Ensure the .prisma folder and binaries are traced
+    './node_modules/@prisma/client': ['./.prisma'],
+    './prisma': ['./'],
+  },
+
+  webpack: (config, { isServer }) => {
+    // ✅ Remove this line entirely:
+    // config.externals.push('@prisma/client');
+
+    return config;
+  },
 };
 
-export default nextConfig;
+
+export default nextConfig
